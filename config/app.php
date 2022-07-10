1024984ef9840210b51930b45413508705dde463
zurctrebla <albertcruz@terra.com.br>
2022-07-10 19:28:10 -0300

    package intervention/image install

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Newttech'),
 
     /*
     |--------------------------------------------------------------------------
@@ -174,6 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /**
+         * Intervention\Image\ImageServiceProvider::class
+         */
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -229,6 +233,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        /**
+         * 'Image' => Intervention\Image\Facades\Image::class
+         */
+        'Image' => Intervention\Image\Facades\Image::class
 
     ],
 
