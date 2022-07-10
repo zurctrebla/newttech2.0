a6bde080a6a6f8d404e1e444fa5350ad0d8c50be
zurctrebla <albertcruz@terra.com.br>
2022-07-10 19:40:50 -0300

    wapnen/google-cloud-vision

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnnotationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('annotate', [AnnotationController::class, 'displayForm'])->name('displayForm');
+Route::post('annotate', [AnnotationController::class, 'annotateImage'])->name('annotateImage');
